abb99cfb9eb223323a7f2cd3bda4363289971f38
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-10-07 17:14:01 +0300

    Added go to Sign Up scene functionality

--- src/main/java/com/mykhailopavliuk/controller/SignInController.java
+++ src/main/java/com/mykhailopavliuk/controller/SignInController.java
@@ -3,7 +3,14 @@ package com.mykhailopavliuk.controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SignInController {
 
@@ -18,5 +25,11 @@ public class SignInController {
 
     @FXML
     private JFXButton goToSignUpButton;
+
+    @FXML
+    void handleSignUpButton(ActionEvent event) throws IOException {
+        Stage stageTheEventSourceNodeBelongs = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stageTheEventSourceNodeBelongs.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/sign-up.fxml"))));
+    }
 
 }
