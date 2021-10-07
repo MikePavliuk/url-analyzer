641bae4a252ce609325cf479015d1376559f98de
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-10-07 08:15:09 +0300

    Set up project

---
+++ src/main/java/com/mykhailopavliuk/UrlAnalyzerApplication.java
@@ -0,0 +1,20 @@
+package com.mykhailopavliuk;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class UrlAnalyzerApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/sign-in.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/mykhailopavliuk/controller/SignInController.java
@@ -0,0 +1,22 @@
+package com.mykhailopavliuk.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignInController {
+
+    @FXML
+    private Button goToSignUpButton;
+
+    @FXML
+    private TextField inputEmail;
+
+    @FXML
+    private PasswordField inputPassword;
+
+    @FXML
+    private Button signInButton;
+
+}
