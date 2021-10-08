0ab5bd673e18b1dc778780a257d1bcbc2fe25b34
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-10-08 20:32:37 +0300

    Created raw User DTO

---
+++ src/main/java/com/mykhailopavliuk/dto/UserDTO.java
@@ -0,0 +1,27 @@
+package com.mykhailopavliuk.dto;
+
+public class UserDTO {
+    private String email;
+    private char[] password;
+
+    public UserDTO(String email, char[] password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char[] getPassword() {
+        return password;
+    }
+
+    public void setPassword(char[] password) {
+        this.password = password;
+    }
+}
