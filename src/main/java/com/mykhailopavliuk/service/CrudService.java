f3eb84777ad0abae594a1cd482202e0f77e7cd17
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-10-09 11:58:01 +0300

    Changed interfaces signature

--- src/main/java/com/mykhailopavliuk/service/CrudService.java
+++ src/main/java/com/mykhailopavliuk/service/CrudService.java
@@ -6,6 +6,6 @@ public interface CrudService <T, ID> {
     T create(T entity);
     T readById(ID id);
     T update(T entity);
-    void delete(ID id);
+    void deleteById(ID id);
     List<T> getAll();
 }
--- src/main/java/com/mykhailopavliuk/service/UserService.java
+++ src/main/java/com/mykhailopavliuk/service/UserService.java
@@ -3,4 +3,5 @@ package com.mykhailopavliuk.service;
 import com.mykhailopavliuk.model.User;
 
 public interface UserService extends CrudService<User, Long> {
+    long getAvailableId();
 }
