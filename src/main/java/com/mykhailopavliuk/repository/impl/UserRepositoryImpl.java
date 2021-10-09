806a51e097144d4429c748674da70a2a6b05889a
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-10-09 11:58:52 +0300

    Implemented repository

--- src/main/java/com/mykhailopavliuk/repository/impl/UserRepositoryImpl.java
+++ src/main/java/com/mykhailopavliuk/repository/impl/UserRepositoryImpl.java
@@ -4,9 +4,9 @@ import com.mykhailopavliuk.model.User;
 import com.mykhailopavliuk.repository.UserRepository;
 import org.springframework.stereotype.Repository;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,6 +14,20 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public <S extends User> S save(S user) {
+        writeToDatabase(user);
+
+        return user;
+    }
+
+    @Override
+    public <S extends User> Iterable<S> saveAll(Iterable<S> users) {
+        for (User user : users) {
+            writeToDatabase(user);
+        }
+        return null;
+    }
+
+    private void writeToDatabase(User user) {
         try (var writer = new BufferedWriter(new FileWriter("database.csv", true))) {
             writer.append(String.valueOf(user.getId()));
             writer.append(",");
@@ -25,57 +39,147 @@ public class UserRepositoryImpl implements UserRepository {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        return user;
     }
 
     @Override
-    public <S extends User> Iterable<S> saveAll(Iterable<S> entities) {
-        return null;
-    }
+    public Optional<User> findById(Long id) {
+        String line;
+        String[] userData;
 
-    @Override
-    public Optional<User> findById(Long aLong) {
+        try (var reader = new BufferedReader(new FileReader("database.csv"))) {
+            while ((line = reader.readLine()) != null) {
+                userData = line.split(",");
+
+                if (userData[0].equals(id.toString())) {
+                    return Optional.of(new User(Long.parseLong(userData[0]), userData[1], userData[2].toCharArray(), null));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return Optional.empty();
     }
 
     @Override
-    public boolean existsById(Long aLong) {
+    public boolean existsById(Long id) {
+        String line;
+        String[] userData;
+
+        try (var reader = new BufferedReader(new FileReader("database.csv"))) {
+            while ((line = reader.readLine()) != null) {
+                userData = line.split(",");
+
+                if (userData[0].equals(id.toString())) {
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return false;
     }
 
     @Override
     public Iterable<User> findAll() {
-        return null;
+        String line;
+        String[] userData;
+        List<User> users = new ArrayList<>();
+
+        try (var reader = new BufferedReader(new FileReader("database.csv"))) {
+            while ((line = reader.readLine()) != null) {
+                userData = line.split(",");
+                users.add(new User(Long.parseLong(userData[0]), userData[1], userData[2].toCharArray(), null));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return users;
     }
 
     @Override
     public long count() {
-        return 0;
+        long count = 0;
+
+        try (var reader = new BufferedReader(new FileReader("database.csv"))) {
+            while (reader.readLine() != null) {
+                count++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return count;
     }
 
     @Override
-    public void deleteById(Long aLong) {
+    public void deleteById(Long id) {
+        String line;
+        String[] userData;
+        StringBuilder updatedFile = new StringBuilder();
 
-    }
+        try (var reader = new BufferedReader(new FileReader("database.csv"))) {
+            while ((line = reader.readLine()) != null) {
+                userData = line.split(",");
 
-    @Override
-    public void delete(User entity) {
+                if (!userData[0].equals(id.toString())) {
+                    updatedFile.append(line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-    }
-
-    @Override
-    public void deleteAll(Iterable<? extends User> entities) {
-
+        try (var writer = new BufferedWriter(new FileWriter("database.csv"))) {
+            writer.write(updatedFile.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void deleteAll() {
-
+        try (var writer = new BufferedWriter(new FileWriter("database.csv"))) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public User findByEmail(String email) {
-        return null;
+    public Optional<User> findByEmail(String email) {
+        String line;
+        String[] userData;
+
+        try (var reader = new BufferedReader(new FileReader("database.csv"))) {
+            while ((line = reader.readLine()) != null) {
+                userData = line.split(",");
+
+                if (userData[1].equals(email)) {
+                    return Optional.of(new User(Long.parseLong(userData[0]), userData[1], userData[2].toCharArray(), null));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public long getAvailableId() {
+        String line;
+        String[] userData = null;
+
+        try (var reader = new BufferedReader(new FileReader("database.csv"))) {
+            while ((line = reader.readLine()) != null) {
+                userData = line.split(",");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return userData != null ? (Long.parseLong(userData[0]) + 1) : 1;
     }
 }
