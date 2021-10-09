31479cd93fdc4f53686a21b28a46ea8c2d1343f6
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-10-09 12:06:14 +0300

    Created exception for identifying entity was not found

---
+++ src/main/java/com/mykhailopavliuk/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.mykhailopavliuk.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException() {    }
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
