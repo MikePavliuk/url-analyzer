b1560f9d67e1581ac88ac8cb49eb3ed8cd7137ed
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-10-11 15:04:33 +0300

    Implemented logic of Sign in page

--- src/main/java/com/mykhailopavliuk/controller/SignInController.java
+++ src/main/java/com/mykhailopavliuk/controller/SignInController.java
@@ -1,27 +1,41 @@
 package com.mykhailopavliuk.controller;
 
+import com.github.plushaze.traynotification.animations.Animations;
+import com.github.plushaze.traynotification.notification.Notifications;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import com.mykhailopavliuk.exception.DatabaseOperationException;
+import com.mykhailopavliuk.exception.EntityNotFoundException;
 import com.mykhailopavliuk.model.User;
 import com.mykhailopavliuk.service.UserService;
+import com.mykhailopavliuk.util.TrayHandler;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import net.rgielen.fxweaver.core.FxWeaver;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.ResourceBundle;
 
 @Component
 @FxmlView("/view/sign-in.fxml")
-public class SignInController {
+public class SignInController implements Initializable {
 
     private final UserService userService;
 
@@ -34,10 +48,7 @@ public class SignInController {
     private JFXPasswordField inputPassword;
 
     @FXML
-    private JFXButton signInButton;
-
-    @FXML
-    private JFXButton goToSignUpButton;
+    private Label singInFailedLabel;
 
     @Autowired
     public SignInController(UserService userService, FxWeaver fxWeaver) {
@@ -45,10 +56,62 @@ public class SignInController {
         this.fxWeaver = fxWeaver;
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        singInFailedLabel.setVisible(false);
+    }
+
     @FXML
-    void handleSignUpButton(ActionEvent event) {
+    void handleSignInButton(ActionEvent event) {
+        if (inputEmail.getText().isEmpty() || inputPassword.getText().isEmpty()) return;
+
+        String email = inputEmail.getText();
+        String password = inputPassword.getText();
+
+        try {
+            User user = userService.readByEmail(email);
+
+            if (Arrays.equals(user.getPassword(), password.toCharArray())) {
+                TrayHandler.notify(
+                        "Welcome, " + user.getEmail(),
+                        "Now you can analyze urls you want",
+                        Notifications.SUCCESS,
+                        Animations.POPUP,
+                        Paint.valueOf("#4883db"),
+                        Duration.seconds(5)
+                );
+                Stage stageTheEventSourceNodeBelongs = (Stage) ((Node)event.getSource()).getScene().getWindow();
+                DashboardMainController dashboardMainController = fxWeaver.loadController(DashboardMainController.class);
+                dashboardMainController.setUser(user);
+                stageTheEventSourceNodeBelongs.setScene(new Scene(fxWeaver.loadView(DashboardMainController.class)));
+            } else {
+                throw new EntityNotFoundException();
+            }
+
+        } catch (EntityNotFoundException exception) {
+            singInFailedLabel.setVisible(true);
+            Timeline timeline = new Timeline();
+            timeline.getKeyFrames().add(
+                    new KeyFrame(Duration.seconds(3), (actionEvent) -> singInFailedLabel.setVisible(false))
+            );
+            timeline.play();
+            inputPassword.setText("");
+
+        } catch (DatabaseOperationException exception) {
+            TrayHandler.notify(
+                    "Error",
+                    exception.getMessage(),
+                    Notifications.ERROR,
+                    Animations.POPUP,
+                    Paint.valueOf("#fc5b5b"),
+                    Duration.seconds(3)
+            );
+        }
+    }
+
+    @FXML
+    void handleGoToSignUpButton(ActionEvent event) {
         Stage stageTheEventSourceNodeBelongs = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        userService.create(new User(1, "email", new char[]{'1', '3', '4'}, null));
         stageTheEventSourceNodeBelongs.setScene(new Scene(fxWeaver.loadView(SignUpController.class)));
     }
 }
