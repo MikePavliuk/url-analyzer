6a6a71b6edfc28eba9b9241c43022c237a31d4e3
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-10-20 16:58:23 +0300

    Implemented tray icon usage

--- src/main/java/com/mykhailopavliuk/UrlAnalyzerApplication.java
+++ src/main/java/com/mykhailopavliuk/UrlAnalyzerApplication.java
@@ -1,13 +1,18 @@
 package com.mykhailopavliuk;
+import com.dustinredmond.fxtrayicon.FXTrayIcon;
 import com.mykhailopavliuk.controller.SignInController;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import net.rgielen.fxweaver.core.FxWeaver;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.swing.*;
+import java.util.Objects;
 
 public class UrlAnalyzerApplication extends Application {
 
@@ -19,6 +24,7 @@ public class UrlAnalyzerApplication extends Application {
 
         context = new SpringApplicationBuilder()
                 .sources(SpringApplication.class)
+                .headless(false)
                 .run(args);
     }
 
@@ -26,7 +32,38 @@ public class UrlAnalyzerApplication extends Application {
     public void start(Stage primaryStage) {
         FxWeaver fxWeaver = context.getBean(FxWeaver.class);
         Parent root = fxWeaver.loadView(SignInController.class);
+
+        primaryStage.getIcons().add(new Image("/assets/url-analyzer-icon-tray-blue-200.png"));
+        try {
+            java.awt.Image image = new ImageIcon(getClass().getResource("/assets/url-analyzer-icon-tray-blue-200.png"), "URL Analyzer").getImage();
+            com.apple.eawt.Application.getApplication().setDockIconImage(image);
+        } catch (Exception e) {
+            // Won't work on Windows or Linux.
+        }
+
+        Platform.setImplicitExit(false);
+        primaryStage.setOnCloseRequest(e -> {
+            e.consume();
+            primaryStage.toBack();
+        });
+
+        FXTrayIcon icon = new FXTrayIcon.Builder(primaryStage, Objects.requireNonNull(getClass().getResource("/assets/url-analyzer-icon-tray-white-150.png")))
+                .menuItem(
+                        "Show", e -> primaryStage.toFront()
+                )
+                .menuItem(
+                        "Exit", e ->  {
+                            this.context.close();
+                            Platform.exit();
+                            System.exit(0);
+                        }
+                )
+                .show()
+                .build();
+
+
         Scene scene = new Scene(root);
+        primaryStage.setTitle("URL Analyzer");
         primaryStage.setScene(scene);
         primaryStage.show();
         primaryStage.centerOnScreen();
