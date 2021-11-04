34cf9193093d737f2dcd28a89163f158c859f387
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-11-04 19:36:52 +0200

    Changed implementation for using resource loader

--- src/main/java/com/mykhailopavliuk/configuration/spring/UrlAnalyzerConfiguration.java
+++ src/main/java/com/mykhailopavliuk/configuration/spring/UrlAnalyzerConfiguration.java
@@ -1,12 +1,16 @@
 package com.mykhailopavliuk.configuration.spring;
 
 import com.mykhailopavliuk.configuration.application.AdminProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,52 +20,47 @@ import java.nio.file.Paths;
 @Profile({ "production" })
 public class UrlAnalyzerConfiguration {
 
+    private final ResourceLoader resourceLoader;
+
+    @Autowired
+    public UrlAnalyzerConfiguration(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
     @Bean
     @Qualifier("usersDatabase")
     public Path getUsersDatabaseFile() throws IOException {
-        Path userDatabase = Paths.get("src/main/resources/database/users.csv");
-
-        if (!Files.exists(userDatabase)) {
-            Files.createFile(userDatabase);
-        }
-
-        return userDatabase;
+        return resourceLoader.getResource("classpath:database/users.csv").getFile().toPath();
     }
 
     @Bean
     @Qualifier("urlsDatabase")
     public Path getUrlsDatabaseFile() throws IOException {
-        Path urlsDatabase = Paths.get("src/main/resources/database/urls.csv");
-
-        if (!Files.exists(urlsDatabase)) {
-            Files.createFile(urlsDatabase);
-        }
-
-        return urlsDatabase;
+        return resourceLoader.getResource("classpath:database/urls.csv").getFile().toPath();
     }
 
     @Bean
     @Qualifier("user_urlDatabase")
     public Path getUserUrlDatabaseFile() throws IOException {
-        Path user_urlDatabase = Paths.get("src/main/resources/database/user-url.csv");
+        return resourceLoader.getResource("classpath:database/urls.csv").getFile().toPath();
+    }
 
-        if (!Files.exists(user_urlDatabase)) {
-            Files.createFile(user_urlDatabase);
-        }
+    @Bean
+    @Qualifier("defaultSettings")
+    public Path getDefaultSettingsFile() throws IOException {
+        return resourceLoader.getResource("classpath:database/user-url.csv").getFile().toPath();
+    }
 
-        return user_urlDatabase;
+    @Bean
+    @Qualifier("settingsSchema")
+    public Path getSettingsSchemaFile() throws IOException {
+        return resourceLoader.getResource("classpath:settings/settings.xsd").getFile().toPath();
     }
 
     @Bean
     @Qualifier("settings")
-    public Path getSettingsFile() throws IOException {
-        Path settings = Paths.get("src/main/resources/settings/settings.xml");
-
-        if (!Files.exists(settings)) {
-            Files.createFile(settings);
-        }
-
-        return settings;
+    public Path getSettingsFile() {
+        return Path.of(".").toAbsolutePath().resolve("settings.xml");
     }
 
     @Bean
