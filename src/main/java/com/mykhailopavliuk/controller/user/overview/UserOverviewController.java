4d28d3e3bdb9e0feeebdbf6bf402c6d560bf688c
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-11-20 17:39:04 +0200

    Implemented logic of statistics calculation and displaying on chart

--- src/main/java/com/mykhailopavliuk/controller/user/overview/UserOverviewController.java
+++ src/main/java/com/mykhailopavliuk/controller/user/overview/UserOverviewController.java
@@ -1,47 +1,59 @@
 package com.mykhailopavliuk.controller.user.overview;
 
-import com.github.plushaze.traynotification.animations.Animations;
-import com.github.plushaze.traynotification.notification.Notifications;
 import com.mykhailopavliuk.controller.SignInController;
 import com.mykhailopavliuk.controller.user.settings.UserSettingsController;
 import com.mykhailopavliuk.controller.user.urls.UserUrlsController;
+import com.mykhailopavliuk.model.Url;
 import com.mykhailopavliuk.model.User;
 import com.mykhailopavliuk.service.UserService;
-import com.mykhailopavliuk.util.TrayNotificationHandler;
+import com.mykhailopavliuk.util.urlHandler.PingStatistics;
+import com.mykhailopavliuk.util.urlHandler.Response;
 import com.mykhailopavliuk.util.urlHandler.UrlHandler;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.chart.*;
 import javafx.scene.control.Label;
-import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 import net.rgielen.fxweaver.core.FxWeaver;
-import net.rgielen.fxweaver.core.FxmlView;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public abstract class UserOverviewController implements Initializable {
-
     private User user;
-
-    private final UserService userService;
-
     private final FxWeaver fxWeaver;
+    private Map<Long, PingStatistics> pingStatisticsMap;
+    private PingStatistics globalPingStatistics;
 
     @FXML
     private Label userEmailLabel;
 
+    @FXML
+    private Label totalNumberOfRequestsLabel;
 
-    public UserOverviewController(UserService userService, FxWeaver fxWeaver) {
-        this.userService = userService;
+    @FXML
+    private Label slowestResponseTimeLabel;
+
+    @FXML
+    private Label fastestResponseTimeLabel;
+
+    @FXML
+    private Label averageResponseTimeLabel;
+
+    @FXML
+    private AreaChart<String, Number> responseChart;
+
+    @FXML
+    private CategoryAxis xAxis;
+
+    @FXML
+    private NumberAxis yAxis;
+
+
+    public UserOverviewController(FxWeaver fxWeaver) {
         this.fxWeaver = fxWeaver;
     }
 
@@ -49,6 +61,62 @@ public abstract class UserOverviewController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         user = fxWeaver.loadController(SignInController.class).getSignedInUser();
         userEmailLabel.setText(user.getEmail());
+        pingStatisticsMap = new HashMap<>();
+        initializeStatistics();
+        initializeChart();
+    }
+
+    private void initializeStatistics() {
+        List<Response> globalResponses = new ArrayList<>();
+        for (Url url : user.getUrls()) {
+            List<Response> responses = UrlHandler.getAllResponsesByUrlId(url.getId());
+            if (!responses.isEmpty()) {
+                pingStatisticsMap.put(url.getId(), new PingStatistics(responses));
+                globalResponses.addAll(responses);
+            }
+        }
+        if (!globalResponses.isEmpty()) {
+            globalPingStatistics = new PingStatistics(globalResponses);
+
+            totalNumberOfRequestsLabel.setText(String.valueOf(globalPingStatistics.getTotalNumberOfRequests()));
+            slowestResponseTimeLabel.setText(globalPingStatistics.getSlowestResponseTime().toMillis() + "ms");
+            fastestResponseTimeLabel.setText(globalPingStatistics.getFastestResponseTime().toMillis() + "ms");
+            averageResponseTimeLabel.setText(globalPingStatistics.getAverageResponseTime().toMillis() + "ms");
+        } else {
+            totalNumberOfRequestsLabel.setText("0");
+            slowestResponseTimeLabel.setText("-");
+            fastestResponseTimeLabel.setText("-");
+            averageResponseTimeLabel.setText("-");
+        }
+
+    }
+
+    private void initializeChart() {
+        xAxis.setLabel("Url id");
+        yAxis.setLabel("Response ms");
+        yAxis.setAutoRanging(false);
+        yAxis.setLowerBound(0);
+        yAxis.setUpperBound(6000);
+
+        XYChart.Series<String, Number> seriesSlow = new XYChart.Series<>();
+        XYChart.Series<String, Number> seriesFast = new XYChart.Series<>();
+        XYChart.Series<String, Number> seriesAverage = new XYChart.Series<>();
+
+        seriesSlow.setName("Slowest");
+        seriesFast.setName("Fastest");
+        seriesAverage.setName("Average");
+
+        pingStatisticsMap.forEach((key,value) -> {
+            if (value != null) {
+                seriesSlow.getData().add(new XYChart.Data<>(String.valueOf(key), value.getSlowestResponseTime().toMillis()));
+                seriesFast.getData().add(new XYChart.Data<>(String.valueOf(key), value.getFastestResponseTime().toMillis()));
+                seriesAverage.getData().add(new XYChart.Data<>(String.valueOf(key), value.getAverageResponseTime().toMillis()));
+            }
+        });
+
+        responseChart.getData().add(seriesSlow);
+        responseChart.getData().add(seriesFast);
+        responseChart.getData().add(seriesAverage);
     }
 
     @FXML
