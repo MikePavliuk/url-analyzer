77b5de00dfeeedb82d31875a2a593aeaafc01cec
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-11-20 18:32:25 +0200

    Added addition field - Url for exporting posibility

--- src/main/java/com/mykhailopavliuk/util/urlHandler/PingStatistics.java
+++ src/main/java/com/mykhailopavliuk/util/urlHandler/PingStatistics.java
@@ -1,15 +1,17 @@
 package com.mykhailopavliuk.util.urlHandler;
 
+import com.mykhailopavliuk.model.Url;
+
 import java.time.Duration;
 import java.util.List;
 
 public class PingStatistics {
+    private Url url;
     private int totalNumberOfRequests;
     private int numberOfNotTimeoutResponses;
     private Duration fastestResponseTime = Duration.ofSeconds(5);
     private Duration slowestResponseTime = Duration.ofSeconds(0);
     private Duration averageResponseTime;
-
 
     public PingStatistics(List<Response> responses) {
         double sumOfDurations = 0;
@@ -31,6 +33,15 @@ public class PingStatistics {
             totalNumberOfRequests++;
         }
         averageResponseTime = Duration.ofMillis(Double.valueOf(sumOfDurations / (totalNumberOfRequests - numberOfNotTimeoutResponses)).longValue());
+    }
+
+    public PingStatistics(Url url, List<Response> responses) {
+        this(responses);
+        this.url = url;
+    }
+
+    public Url getUrl() {
+        return url;
     }
 
     public int getTotalNumberOfRequests() {
