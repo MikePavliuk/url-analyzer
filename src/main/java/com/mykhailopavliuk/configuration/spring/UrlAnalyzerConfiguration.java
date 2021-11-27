402199a5bb3344066f454828d566629e9e13c266
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-11-27 12:04:23 +0200

    Changed getting databases from src to resource loading one

--- src/main/java/com/mykhailopavliuk/configuration/spring/UrlAnalyzerConfiguration.java
+++ src/main/java/com/mykhailopavliuk/configuration/spring/UrlAnalyzerConfiguration.java
@@ -7,14 +7,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Configuration
 @Profile({ "production" })
@@ -30,37 +26,21 @@ public class UrlAnalyzerConfiguration {
     @Bean
     @Qualifier("usersDatabase")
     public Path getUsersDatabaseFile() throws IOException {
-        Path userDatabase = Paths.get("src/main/resources/database/users.csv");
-
-        if (!Files.exists(userDatabase)) {
-            Files.createFile(userDatabase);
-        }
-
-        return userDatabase;
+        return resourceLoader.getResource("classpath:database/users.csv").getFile().toPath();
     }
 
     @Bean
     @Qualifier("urlsDatabase")
     public Path getUrlsDatabaseFile() throws IOException {
-        Path urlsDatabase = Paths.get("src/main/resources/database/urls.csv");
-
-        if (!Files.exists(urlsDatabase)) {
-            Files.createFile(urlsDatabase);
-        }
-
-        return urlsDatabase;
+        return resourceLoader.getResource("classpath:database/urls.csv").getFile().toPath();
     }
 
     @Bean
     @Qualifier("user_urlDatabase")
     public Path getUserUrlDatabaseFile() throws IOException {
-        Path user_urlDatabase = Paths.get("src/main/resources/database/user-url.csv");
-
-        if (!Files.exists(user_urlDatabase)) {
-            Files.createFile(user_urlDatabase);
-        }
-        return user_urlDatabase;
+        return resourceLoader.getResource("classpath:database/user-url.csv").getFile().toPath();
     }
+
     @Bean
     @Qualifier("defaultSettings")
     public Path getDefaultSettingsFile() throws IOException {
